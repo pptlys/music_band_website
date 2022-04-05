fcc89d0a29205f46962d410c67a68083a7685b4d
Zhaojie YANG <pptlys@gmail.com>
2022-04-05 19:53:14 -0400

    fix html and JS

--- scripts/events.js
+++ scripts/events.js
@@ -254,3 +254,14 @@ function calculateDaysUntilEvent(concertDate) {
 
     return daysLeft;
 }
+
+function lang_toggle(){
+    let lang_toggle = document.getElementById("lang-toggle")
+    let current_lang = document.documentElement.lang
+    
+    if (current_lang === 'en') {
+        window.open("/index_fr.html", "_self")
+    } else {
+        window.open("/index.html", "_self")
+    }
+}
