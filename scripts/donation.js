546a60ad81db7af6f7576d494243b97f2ea8428b
Zhaojie YANG <pptlys@gmail.com>
2022-04-06 18:16:29 -0400

    create donation_fr.html

--- scripts/donation.js
+++ scripts/donation.js
@@ -1,11 +1,10 @@
 function lang_toggle(){
-    let lang_toggle = document.getElementById("lang-toggle");
     let current_lang = document.documentElement.lang;
     
     if (current_lang === 'en') {
-        window.open("../index_fr.html", "_self");
+        window.open("./donation_fr.html", "_self");
     } else {
-        window.open("../index.html", "_self");
+        window.open("./donation_en.html", "_self");
     }
 }
 
